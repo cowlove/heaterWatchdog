b1b8df6f05e4c877d7100d79f43cb634cb9d3635
jim <jim@vheavy.com>
2022-03-17 09:57:06 -0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,31 @@
+BOARD=esp32doit-devkit-v1
+#BOARD=heltec_wifi_lora_32
+#BOARD=nodemcu-32s
+VERBOSE=1
+MONITOR_SPEED=921600
+
+GIT_VERSION := "$(shell git describe --abbrev=4 --dirty --always --tags)"
+BUILD_EXTRA_FLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+
+backtrace:
+	tr ' ' '\n' | /home/jim/.arduino15/packages/esp32/tools/xtensa-esp32-elf-gcc/*/bin/xtensa-esp32-elf-addr2line -f -i -e ${BUILD_DIR}/${MAIN_NAME}.elf
+	
+CHIP=esp32
+OTA_ADDR=192.168.4.148
+IGNORE_STATE=1
+
+include ${HOME}/Arduino/libraries/makeEspArduino/makeEspArduino.mk
+
+fixtty:
+	stty -F ${UPLOAD_PORT} -hupcl -crtscts -echo raw  ${MONITOR_SPEED}
+
+cat:	fixtty
+	cat ${UPLOAD_PORT}
+
+socat:  
+	socat udp-recv:9000 - 
+
+curl: ${BUILD_DIR}/${MAIN_NAME}.bin
+	curl -v --limit-rate 10k --progress-bar -F "image=@${BUILD_DIR}/${MAIN_NAME}.bin" ${OTA_ADDR}/update 
+
+
---
+++ esp32template.ino
@@ -0,0 +1,128 @@
+#include "jimlib.h"
+//#include <heltec.h>	
+
+JimWiFi jw;
+EggTimer sec(100);
+//HardwareSerial Serial3(2);
+
+struct {
+	int led = 2;
+	int tx = 16;
+	int rx = 17;
+	int dummy = 21;
+} pins;
+
+void setup() {
+	esp_task_wdt_init(20, true);
+	esp_task_wdt_add(NULL);
+	//Heltec.begin(true, true, true, true, 915E6);
+	pinMode(pins.led, OUTPUT);
+	pinMode(pins.rx, INPUT);
+	pinMode(pins.tx, INPUT);
+	digitalWrite(pins.led, 1);
+	Serial.begin(921600, SERIAL_8N1);
+	jw.onConnect([](void) {});
+	jw.onOTA([](void) {});
+	Serial2.begin(4800, SERIAL_8N1, pins.rx, pins.dummy, true);
+//	Serial3.begin(4800, SERIAL_8N1, pins.tx, pins.dummy, true);
+	Serial2.setTimeout(1);
+//	Serial3.setTimeout(1);
+}
+
+int old_rx, old_tx, rx_transitions, tx_transitions, rx_bytes;
+
+
+std::string hexdump(const char *in, int len) {
+	std::string r;
+	char b[3]; 
+	for (int n = 0; n < len; n++) { 
+		sprintf(b, "%02x", in[n]);
+		r += b;
+	}
+	return r;
+}
+
+
+class SerialChunker {
+	HardwareSerial &ser;
+	uint8_t buf[256];
+	int l = 0, timeout = 0, startTime = 0;
+	uint32_t lastRead = 0;
+public:
+	SerialChunker(HardwareSerial &s, int ms = 50) : ser(s), timeout(ms) { }
+
+	bool check(std::function<void(const char *, int, int)> f) { 
+		uint32_t ms = millis();
+		if (ser.available()) {
+			int n = ser.readBytes(buf + l, sizeof(buf) - l);
+			if (n > 0 && l == 0) { 
+				startTime = ms;
+			}
+			l += n;
+			lastRead = ms;
+		}
+		if (ms - lastRead > timeout && l > 0) {
+			f((const char *)buf, l, startTime);
+			l = 0;
+		} 
+	}
+};
+
+uint32_t lastRec; 
+
+SerialChunker sc2(Serial2);
+//SerialChunker sc3(Serial3);
+
+void loop() {
+	esp_task_wdt_reset();
+	jw.run();
+	if (sec.tick()) {
+		digitalWrite(pins.led, !digitalRead(pins.led));
+		std::string s = strfmt("transitions rx: %d  bytes\n", rx_bytes);
+		//Serial.print(s.c_str());
+		jw.udpDebug(s.c_str());
+	}
+	int rx = digitalRead(pins.rx);
+	if (rx != old_rx) rx_transitions++;
+	old_rx = rx;
+
+	int tx = digitalRead(pins.tx);
+	if (tx != old_tx) tx_transitions++;
+	old_tx = tx;
+
+
+	sc2.check([](const char *b, int l, int t) {
+		std::string s = strfmt("S2 %04d: ", t % 10000) + hexdump(b, l);
+		Serial.println(s.c_str());
+		jw.udpDebug(s.c_str());
+	});
+
+/*
+	sc3.check([](const char *b, int l, int t) {
+		char hexbuf[2048];
+		hexdump(b, l, hexbuf);
+		std::string s = strfmt("S3 %04d: %s", t % 10000, hexbuf);
+		Serial.println(s.c_str());
+		jw.udpDebug(s.c_str());
+	});
+*/
+#if 0
+	if (Serial2.available()) {
+		int ms = millis();
+		if (ms - lastRec > 50) {
+			Serial.printf("\n%04d: ", (int)(ms % 10000));
+		}
+		lastRec = ms;
+		char buf[1024];
+		int n = Serial2.readBytes((uint8_t *)buf, sizeof(buf));
+		rx_bytes += n;
+		char hexbuf[2048];
+		hexdump(buf, n, hexbuf);
+		std::string s = hexbuf;
+		//jw.udpDebug(s.c_str());
+		Serial.print(s.c_str());
+	}
+#endif
+	//delay(1);
+}
+
