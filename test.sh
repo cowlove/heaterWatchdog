0a04c36cdb730209606b2a55914ade5e803136c5
Jim Evans <jim@vheavy.com>
2022-11-22 08:33:26 -0800

    Fix test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ EXPECTED=testexpected.out
 
 #rm $PROG
 
-make $PROG && $PROG --seconds 600  | tail -1 > $OUT &&\
+make $PROG > /dev/null && $PROG --seconds 600  | tail -1 > $OUT &&\
 tail -1 $OUT | diff - $EXPECTED
 
 
